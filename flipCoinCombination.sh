fcbf9b7389fadd5459452cb49f8ae76c0a7cd587
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-18 18:35:09 +0530

    Add: Flipped two coins and calculated percentage of doublets HH, HT, TH and TT

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,29 +1,42 @@
 #!/bin/bash -x
 Heads=0
 Tails=1
-headCount=0
-tailCount=0
+head_HeadCount=0 # HH
+tail_TailCount=0 # TT
+head_TailCount=0 # HT
+tail_HeadCount=0 # TH
 iterations=0
 
 declare -A storage
 
 while [ $iterations -lt 10 ]
 do
-	result=$((RANDOM%2))
-	if [ $result -eq $Heads ]
+	coin_1=$((RANDOM%2))
+	coin_2=$((RANDOM%2))
+	if [[ $coin_1 -eq $Heads && $coin_2 -eq $Heads ]]
 	then
-		((headCount++))
+		((head_HeadCount++))
+	elif [[ $coin_1 -eq $Tails && $coin_2 -eq $Tails ]]
+	then
+		((tail_TailCount++))
+	elif [[ $coin_1 -eq $Heads && $coin_2 -eq $Tails ]]
+	then
+		((head_TailCount++))
 	else
-		((tailCount++))
+		((tail_HeadCount++))
 	fi
 	((iterations++))
 done
 
-storage[heads]=$headCount
-storage[tails]=$tailCount
+storage[HH]=$head_HeadCount
+storage[TT]=$tail_TailCount
+storage[HT]=$head_TailCount
+storage[TH]=$tail_HeadCount
 
 echo ${storage[@]}
 
-percentageOfSingletHeads=`echo "${storage[heads]}*100/$iterations" | bc`
-percentageOfSingletTails=`echo "${storage[tails]}*100/$iterations" | bc`
+percentageOfHead_HeadCombo=`echo "${storage[HH]}*100/$iterations" | bc`
+percentageOfTail_TailCombo=`echo "${storage[TT]}*100/$iterations" | bc`
+percentageOfHead_TailCombo=`echo "${storage[HT]}*100/$iterations" | bc`
+percentageOfTail_HeadCombo=`echo "${storage[TH]}*100/$iterations" | bc`
 
