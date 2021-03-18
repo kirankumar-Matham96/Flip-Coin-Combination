b37470d0697486fcd197f885aeb8eb54f3456def
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-18 19:08:03 +0530

    Add: Flipped three coind and calculated percentages of triplets: HHH,HHT,HTH,THH,TTT,TTH,THT AND HTT

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -x
 Heads=0
 Tails=1
-head_HeadCount=0 # HH
-tail_TailCount=0 # TT
-head_TailCount=0 # HT
-tail_HeadCount=0 # TH
+HHH_Count=0 # HHH
+HHT_Count=0 # HHT
+HTH_Count=0 # HTH
+THH_Count=0 # THH
+TTT_Count=0 # TTT
+TTH_Count=0 # TTH
+THT_Count=0 # THT
+HTT_Count=0 # HTT
 iterations=0
 
 declare -A storage
@@ -13,30 +17,56 @@ while [ $iterations -lt 10 ]
 do
 	coin_1=$((RANDOM%2))
 	coin_2=$((RANDOM%2))
-	if [[ $coin_1 -eq $Heads && $coin_2 -eq $Heads ]]
+	coin_3=$((RANDOM%2))
+	if [[ $coin_1 -eq $Heads && $coin_2 -eq $Heads && $coin_3 -eq $Heads ]]
 	then
-		((head_HeadCount++))
-	elif [[ $coin_1 -eq $Tails && $coin_2 -eq $Tails ]]
+		((HHH_Count++))
+	elif [[ $coin_1 -eq $Heads && $coin_2 -eq $Heads && $coin_3 -eq $Tails ]]
 	then
-		((tail_TailCount++))
-	elif [[ $coin_1 -eq $Heads && $coin_2 -eq $Tails ]]
+		((HHT_Count++))
+
+	elif [[ $coin_1 -eq $Heads && $coin_2 -eq $Tails && $coin_3 -eq $Heads ]]
 	then
-		((head_TailCount++))
+		((HTH_Count++))
+
+	elif [[ $coin_1 -eq $Tails && $coin_2 -eq $Heads && $coin_3 -eq $Heads ]]
+	then
+		((THH_Count++))
+
+	elif [[ $coin_1 -eq $Tails && $coin_2 -eq $Tails && $coin_3 -eq $Tails ]]
+	then
+		((TTT_Count++))
+
+	elif [[ $coin_1 -eq $Tails && $coin_2 -eq $Tails && $coin_3 -eq $Heads ]]
+	then
+		((TTH_Count++))
+
+	elif [[ $coin_1 -eq $Tails && $coin_2 -eq $Heads && $coin_3 -eq $Tails ]]
+	then
+		((THT_Count++))
+
 	else
-		((tail_HeadCount++))
+		((HTT_Count++))
 	fi
 	((iterations++))
 done
 
-storage[HH]=$head_HeadCount
-storage[TT]=$tail_TailCount
-storage[HT]=$head_TailCount
-storage[TH]=$tail_HeadCount
-
+storage[HHH]=$HHH_Count
+storage[HHT]=$HHT_Count
+storage[HTH]=$HTH_Count
+storage[THH]=$THH_Count
+storage[TTT]=$TTT_Count
+storage[TTH]=$TTH_Count
+storage[THT]=$THT_Count
+storage[HTT]=$HTT_Count
 echo ${storage[@]}
 
-percentageOfHead_HeadCombo=`echo "${storage[HH]}*100/$iterations" | bc`
-percentageOfTail_TailCombo=`echo "${storage[TT]}*100/$iterations" | bc`
-percentageOfHead_TailCombo=`echo "${storage[HT]}*100/$iterations" | bc`
-percentageOfTail_HeadCombo=`echo "${storage[TH]}*100/$iterations" | bc`
+percentageOf_HHH_Combo=`echo "${storage[HHH]}*100/$iterations" | bc`
+percentageOf_HHT_Combo=`echo "${storage[HHT]}*100/$iterations" | bc`
+percentageOf_HTH_Combo=`echo "${storage[HTH]}*100/$iterations" | bc`
+percentageOf_THH_Combo=`echo "${storage[THH]}*100/$iterations" | bc`
+percentageOf_TTT_Combo=`echo "${storage[TTT]}*100/$iterations" | bc`
+percentageOf_TTH_Combo=`echo "${storage[TTH]}*100/$iterations" | bc`
+percentageOf_THT_Combo=`echo "${storage[THT]}*100/$iterations" | bc`
+percentageOf_HTT_Combo=`echo "${storage[HTT]}*100/$iterations" | bc`
 
