05cd0249cc0ad8370dc7b399c1b439c393b4c25b
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-03-18 18:04:26 +0530

    Add: Stored in dictionary and calculated percentage of singlet

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,29 @@
 #!/bin/bash -x
 Heads=0
 Tails=1
-result=$((RANDOM%2))
-if [ $result -eq $Heads ]
-then
-	echo "winner: Heads"
-else
-	echo "winner: Tails"
-fi
+headCount=0
+tailCount=0
+iterations=0
+
+declare -A storage
+
+while [ $iterations -lt 10 ]
+do
+	result=$((RANDOM%2))
+	if [ $result -eq $Heads ]
+	then
+		((headCount++))
+	else
+		((tailCount++))
+	fi
+	((iterations++))
+done
+
+storage[heads]=$headCount
+storage[tails]=$tailCount
+
+echo ${storage[@]}
+
+percentageOfSingletHeads=`echo "${storage[heads]}*100/$iterations" | bc`
+percentageOfSingletTails=`echo "${storage[tails]}*100/$iterations" | bc`
+
